5c4a97f35d38063aa67ae17890ba971f7a05d2c9
tareq179 <abdulkarimctg373@gmail.com>
2021-02-14 22:45:32 +0600

    intervewe

---
+++ null-vs-undefined.js
@@ -0,0 +1,14 @@
+// let pakhi;
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+
+
+// const premik = { nsme: "smart dude", phone: 0128595463 };
+// console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2, 5, 9];
+console.log(ages(11));
