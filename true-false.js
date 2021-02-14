96cfaa95aee9a37284961b182c100135baa13d03
tareq179 <abdulkarimctg373@gmail.com>
2021-02-14 22:11:59 +0600

    add true-false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// falsy
+// 0
+// ""
+// undefined
+// null
+// NaN
+// Truthy
+// '0' ' '
+let name = NaN:
+    console.log(name);
+
+if (name) {
+    console.log("condition is true");
+} else {
+    console.log("condition is false")
+}
